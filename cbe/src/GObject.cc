d275b0af6acc634501c7e50379c83782a2a375ab
baueran <baueran>
2001-05-20 17:47:05 +0000

    Minor style and documentation fixes

--- cbe/src/GObject.cc
+++ cbe/src/GObject.cc
@@ -1,28 +1,47 @@
-// GObject.cpp: implementation of the GObject class.
+// GObject.cc - source file for the CBE project
 //
-//////////////////////////////////////////////////////////////////////
+// Original code by George Stetten and Korin Crawford
+// Copyright given to the public domain
+// Please email comments to stetten@acpub.duke.edu
+//
+// Modifications are copyright (c) 2001  Ludwig-Maximilian-Universitaet Muenchen
+//                                       http://www.uni-muenchen.de/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
 
 #include "GObject.h"
-/* This class is the basic class for graphic objects */
-GObject::GObject()
-{
-	hidden = false; // Not hidden at the beginning
-	gl_list = glGenLists( 1 ); // Create a new list for the object
+
+// This class is the basic class for graphic objects
+GObject::GObject() {
+  hidden = false; // Not hidden at the beginning
+  gl_list = glGenLists( 1 ); // Create a new list for the object
 }
 
-GObject::~GObject()
-{
-	glDeleteLists( gl_list, 1 ); /* Delete the GL list corresponding
-		to the object. */
+GObject::~GObject() {
+  glDeleteLists( gl_list, 1 ); // Delete the GL list corresponding to the object.
 }
 
 void GObject::draw() {
-	if ( !hidden ) {
-		glCallList( gl_list ); // Draw the object
-	}
+  if ( !hidden ) {
+    glCallList( gl_list ); // Draw the object
+  }
 }
 
-/* The procedure is empty in this class, but this empty 
-   implementation may not be removed from here, because 
-   it would produce a link error. */
-void GObject::makeList() {}
+// The procedure is empty in this class, but this empty 
+// implementation may not be removed from here, because 
+// it would produce a link error.
+void GObject::makeList() {
+}
--- cbe/src/GObject.h
+++ cbe/src/GObject.h
@@ -1,6 +1,26 @@
-// GObject.h: interface for the GObject class.
+// GObject.h - source file for the CBE project
 //
-//////////////////////////////////////////////////////////////////////
+// Original code by George Stetten and Korin Crawford
+// Copyright given to the public domain
+// Please email comments to stetten@acpub.duke.edu
+//
+// Modifications are copyright (c) 2001  Ludwig-Maximilian-Universitaet Muenchen
+//                                       http://www.uni-muenchen.de/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
 
 #ifndef __GOBJECT_H
 #define __GOBJECT_H
@@ -12,28 +32,35 @@
 #include <GL/gl.h>
 
 class GObject {
-private:
-	GLuint gl_list; // The GL number of the list of this object
-	bool hidden; // Shows, whether the object is hidden
-public:
-	GObject();
-	virtual ~GObject();
-	// Returns the GL number of the list of this object
-	virtual inline GLuint getList() { return gl_list; }
-	/* Creates the graphic representation of the object. It
-	   writes the graphic operations to the GL list with the
-	   number gl_list created in the constructor.
-	   This procedure must be overwritten in each class derived
-	   from GObject. */
-	virtual void makeList() = 0;
-	/* Sets the hidden flag to true. The object will not be
-	   displayed by the next draw(). */
-	virtual inline void hide() { hidden = true; }
-	/* Sets the hidden flag to false. The object will be
-	   displayed by the next draw(). */
-	virtual inline void unhide() { hidden = false; }
-	// Draws the object.
-	virtual void draw();
+ private:
+  GLuint gl_list; // The GL number of the list of this object
+  bool hidden; // Shows, whether the object is hidden
+
+ public:
+  GObject();
+  virtual ~GObject();
+  // Returns the GL number of the list of this object
+  virtual inline GLuint getList() {
+    return gl_list;
+  }
+  // Creates the graphic representation of the object. It
+  // writes the graphic operations to the GL list with the
+  // number gl_list created in the constructor.
+  // This procedure must be overwritten in each class derived
+  // from GObject.
+  virtual void makeList() = 0;
+  // Sets the hidden flag to true. The object will not be
+  // displayed by the next draw().
+  virtual inline void hide() {
+    hidden = true; 
+  }
+  // Sets the hidden flag to false. The object will be
+  // displayed by the next draw().
+  virtual inline void unhide() {
+    hidden = false;
+  }
+  // Draws the object.
+  virtual void draw();
 };
 
 #endif
