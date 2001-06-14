20ae96d2532ee88ea5de3d83cf01ccc74258a201
urbanek <urbanek>
2001-06-14 18:31:49 +0000

    Forgotten new files

---
+++ cbe/src/JoystickDriver.cc
@@ -0,0 +1,77 @@
+// joystick-driver.hh - source file for the CBE project
+// Copyright (c) 2001  Ludwig-Maximilian-Universitaet Muenchen
+//                     http://www.uni-muenchen.de/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+extern "C" {
+#include <linux/joystick.h> // so we need at least the Kernel-Headers
+#include <fcntl.h>  // Needed by open
+#include <unistd.h> // Needed by read
+}
+
+#include "JoystickDriver.hh"
+#include <iostream>
+
+// Constructor
+JoystickDriver::JoystickDriver() {
+  // Initialize Values
+  xAxis=0;
+  yAxis=0;
+  buttons=0;
+  joystickAvailable=false;
+
+  joydev=open("/dev/input/js0", O_RDONLY); // open the first joystick-device
+  if (joydev != -1) {  // ok, joystick available
+    joystickAvailable=true;
+    refreshJoystick();
+  }
+}
+
+// Destructor
+JoystickDriver::~JoystickDriver() {
+}
+
+// just get the jostic-Values into the object
+// here I'm using the old and depreciated 0.x Linux-Joystick-Driver
+// View /usr/src/linux/Documentation/joystick-api.txt on linux-systems for explanation
+void JoystickDriver::refreshJoystick() {
+  struct JS_DATA_TYPE js;
+  if (joystickAvailable) {
+    if (read(joydev, &js, JS_RETURN)!=JS_RETURN)
+      cout << "Problem reading joystick device!" << endl;
+    buttons=js.buttons;
+    xAxis=js.x;
+    yAxis=js.y;
+  }
+}
+
+// Returns x-value
+float JoystickDriver::getXaxis() {
+  //return ((float)xAxis/(float)32767);
+  return((float)(xAxis-128)/128);
+}
+
+// Returns y-value
+float JoystickDriver::getYaxis() {
+  //return ((float)yAxis/(float)32767);
+  return((float)(yAxis-128)/128);
+}
+
+// Returns buttons (first button: button&1, second: button&2 ...)
+int JoystickDriver::getButtons() {
+  return buttons;
+}
---
+++ cbe/src/JoystickDriver.hh
@@ -0,0 +1,38 @@
+// joystick-driver.hh - source file for the CBE project
+// Copyright (c) 2001  Ludwig-Maximilian-Universitaet Muenchen
+//                     http://www.uni-muenchen.de/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+
+// very simple joystick-interface
+class JoystickDriver {
+private:
+  int joydev;
+  int xAxis; // Position der x-Achse
+  int yAxis; // Position der y-Achse
+  int buttons; // Status der Tasten
+  bool joystickAvailable; // true if joystick available
+
+public:
+  JoystickDriver(); // initialises first available joystick
+  ~JoystickDriver();
+
+  void refreshJoystick(); // Reads the new joy-values into the object
+  float getXaxis(); // returns value for the x-axis (-1<=x<=1)
+  float getYaxis(); // returns value for the y-axis (-1<=y<=1)
+  int getButtons(); // returns status of buttons 
+};
