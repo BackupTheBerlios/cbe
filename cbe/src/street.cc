5a3958859b194f7323dc4e5de3158735a5c3710d
loh <loh>
2001-06-15 00:02:53 +0000

    I just made the street a little nicer. Nothing really exciting.

--- cbe/src/street.cc
+++ cbe/src/street.cc
@@ -55,33 +55,37 @@ void Street::writeList() {
   points[1].x=startx+step;
   points[1].y=starty;
   points[1].z=startz;
+  int a;
 
-  for (long n=2;n<length;n+=10) {
+  for (long n=2;n<length;n+=50) {
       int r;
       r=rand()%100;
+      a=0;
       if (r<80)
-	    for (long i=n;i<n+10;i++) {
+	    for (long i=n;i<n+50;i++) {
 	      points[i].x=points[i-1].x+step;
 	      points[i].z=points[i-1].z;
 	      points[i].y=starty;
 		}
 	
     if (r>=80 && r<90)
-	  for (int i=n;i<n+10;i++) {
+	  for (int i=n;i<n+50;i++) {
 	      points[i].x=points[i-1].x+step;
-	      points[i].z=points[i-1].z+step*2;
+	      points[i].z=points[i-1].z+step*sin((double)a/17);
 	      points[i].y=starty;
+	      a++;
 	  }
 	
     if (r>=90)
-	  for (int i=n;i<n+10;i++) {
+	  for (int i=n;i<n+50;i++) {
  	      points[i].x=points[i-1].x+step;
-	      points[i].z=points[i-1].z-step*2;
+	      points[i].z=points[i-1].z-step*sin((double)a/17);
 	      points[i].y=starty;
+	      a++;
 	  }
   }
   
-  glBegin(GL_TRIANGLE_STRIP);
+  glBegin(GL_QUAD_STRIP);
     glColor3f(.3,.3,.3);
     long i;
     for (i=1;i<999;i++) {
@@ -92,12 +96,12 @@ void Street::writeList() {
 		nx=-vz/sqrt(vx*vx+vz*vz);
 		nz=vx/sqrt(vx*vx+vz*vz);
 		//draw street out of triangles
-		glVertex3f(points[i].x+nx*broadness/2,
+		glVertex3f(points[i].x,
 			   points[i].y,
-			   points[i].z+nz*broadness/2);
-		glVertex3f(points[i].x-nx*broadness/2,
+			   points[i].z+broadness/2);
+		glVertex3f(points[i].x,
 			   points[i].y,
-			   points[i].z-nz*broadness/2);
+			   points[i].z-broadness/2);
 	}
   glEnd();
 
@@ -111,9 +115,29 @@ void Street::writeList() {
       nx=-vz/sqrt(vx*vx+vz*vz);
       nz=vx/sqrt(vx*vx+vz*vz);
       
-      createPoles(1,points[i].x+nx*broadness/2,points[i].y,points[i].z+nz*broadness/2+0.2);
-      createPoles(1,points[i].x-nx*broadness/2,points[i].y,points[i].z-nz*broadness/2-0.2);
+      createPoles(1,points[i].x,points[i].y,points[i].z+broadness/2+.5);
+      createPoles(1,points[i].x,points[i].y,points[i].z-broadness/2-.5);
   }
+  
+ // Draw Middlelines
+ glColor3f(.9,.9,.9);
+ for (i=1;i<999;i+=10){
+ 	glBegin(GL_POLYGON);
+		glVertex3f(points[i].x,
+			   points[i].y+.01,
+			   points[i].z+broadness/30);
+		glVertex3f(points[i].x,
+			   points[i].y+.01,
+			   points[i].z-broadness/30);
+		glVertex3f(points[i+6].x,
+			   points[i+6].y+.01,
+			   points[i+6].z-broadness/30);
+		glVertex3f(points[i+6].x,
+			   points[i+6].y+.01,
+			   points[i+6].z+broadness/30);
+	glEnd();
+ } 
+  
 }
 
 
