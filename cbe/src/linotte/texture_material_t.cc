f2085d17aa11c6fc39665e1c90327bf246325d80
bliebl <bliebl>
2002-01-04 21:32:01 +0000

    changed clamp to tile mode

--- cbe/src/linotte/texture_material_t.cc
+++ cbe/src/linotte/texture_material_t.cc
@@ -91,8 +91,8 @@ texture_material_t::texture_material_t(
 		glBindTexture( GL_TEXTURE_2D, m_texture );
 
 		
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		//glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
